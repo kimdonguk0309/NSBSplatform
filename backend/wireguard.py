1cd431631b7ad2148aa9e8485cfeb2f58ee1f108
kimdonguk0309 <vvv861005@gmail.com>
2025-07-24 18:00:02 +0900

    Update wireguard.py

--- backend/wireguard.py
+++ backend/wireguard.py
@@ -8,3 +8,18 @@ def generate_keypair():
     private_key_b64 = base64.b64encode(bytes(private_key)).decode('ascii')
     public_key_b64 = base64.b64encode(bytes(public_key)).decode('ascii')
     return {"private_key": private_key_b64, "public_key": public_key_b64}
+
+def build_client_config(data):
+    config = f"""
+[Interface]
+PrivateKey = {data.client_private}
+Address = 10.0.0.2/24
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = {data.provider_public}
+Endpoint = {data.endpoint}
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 25
+"""
+    return {"wg_config": config}
